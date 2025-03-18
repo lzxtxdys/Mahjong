715891d67bc2b72d38b04af831b66d6147ad5e9a
lzxtxdys <zli32@uci.edu>
2025-03-18 03:14:56 -0700

    modify details

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from game_4AI import MahjongGame
 from rl_agent import RLAgent
 
-# Play a game
 game = MahjongGame()
 
-# Mark some players as AI
 for i, player in enumerate(game.players):
-    if i < 3:  # Make first 3 players AI
+    if i < 3:
         player.is_ai = True
         player.rl_agent = RLAgent()
 
-game.play_game()  # Play one game
+game.play_game()
